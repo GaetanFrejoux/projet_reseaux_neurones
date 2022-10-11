fd601949effda08a665a376161fbacdfa298429a
GaetanFrejoux <gaetanfrejoux@gmail.com>
2022-10-11 14:18:48 +0200

    first commit

---
+++ src/resources.py
@@ -0,0 +1,7 @@
+# Authors : Frejoux Gaetan, Niord Mathieu
+
+# Resources ########################################################
+URL_P2_D1 =          "../res/p2_d1.txt"            # p2_d1.txt
+URL_P2_D2 =          "../res/p2_d2?txt"            # p2_d2.txt
+URL_WANG  =          "../res/WangSignatures.xlsx"  # WangSignatures.xlsx
+####################################################################
