97ac8c5fdea4e6541a8c2e576b2d16d23290981a
GaetanFrejoux <gaetanfrejoux@gmail.com>
2022-10-18 09:37:09 +0200

    Co-authored-by: Mathieu Niord <MathieuNiord@users.noreply.github.com>

--- src/Exercice_01.py
+++ src/Exercice_01.py
@@ -1,58 +1,71 @@
-from distutils.log import error
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from resources import URL_P2_D1, URL_P2_D2
+from resources import (URL_P2_D1, URL_P2_D2, URL_SAVE_RES_E01)
+
+
 ALPHA = 0.1
+
 # 1.1
 
+W_OR = np.array([-0.5, 1, 1])
+W_AND = np.array([-1.5, 1, 1])
+X = np.array([[0,0],[0,1],[1,0],[1,1]])
 
 def perceptron_simple(x, w, active):
     seuil = w[0]
     dot = np.dot(x, w[1:])
     x = seuil + dot
-    if active == 0:
-        return np.sign(x)
-    elif active == 1:
-        return np.tanh(x)
+    return np.sign(x) if (active == 0) else np.tanh(x)
 
-
-x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-w = np.array([-0.5, 1, 1])
 active = 0
-result = perceptron_simple(x, w, active)
+
+result = perceptron_simple(X, W_OR, active)
 # if result[i] = -1 then CLASS 0 else if result[i] = 1 then CLASS 1
-plt.scatter(x[:, 0], x[:, 1], c=result)
+plt.scatter(X[:, 0], X[:, 1], c=result)
 
 # draw the hyperplane
 if active == 0:
-    a0 = np.linspace(-1, 2, 2)
-    a1 = (w[0] + a0*w[1]) / (-w[2])
+    a0 = np.linspace(-1, 2)
+    a1 = (W_OR[0] + a0*W_OR[1]) / (-W_OR[2])
     plt.plot(a0, a1, 'r-')
 elif active == 1:
-    plt.axhline(y=w[0], color='red', linestyle='-')
+    plt.axhline(y=W_OR[0], color='red', linestyle='-')
 
 
-# plt.show()
+plt.show()
+plt.savefig(URL_SAVE_RES_E01 + "1.1.png")
 
+
+def plot_with_class(X,w,c,title):
+    x = np.linspace(-2,2,50)
+    y = (w[0] + x*w[1]) / (-w[2])
+    plt.title(title)
+    plt.plot(x, y)
+    plt.scatter(X[:,0],X[:,1],c=c)
+    plt.grid()
+    plt.show()
 
 # 1.2 Widrow-hoff
 
 def apprentissage_widrow(x, yd, epoch, batch_size):
     w = np.random.randn(3)
     erreur = []
-    for j in range(epoch):
+    for i in range(epoch):
         e = 0
-        for i in range(batch_size):
-            result = perceptron_simple(x[i], w, 0)
-            e += 0.5*(yd[i]-result)**2
-            w[0] = w[0] + ALPHA*(yd[i]-result)*1
-            w[1] = w[1] + ALPHA*(yd[i]-result)*x[i][0]
-            w[2] = w[2] + ALPHA*(yd[i]-result)*x[i][1]
-        if (e == 0):
-            print('Weights are found : ', w)
-            break
+        for j in range(len(x)):
+            result = perceptron_simple(x[j], w, 0)
+            e += (yd[i] - result)**2
+            
+            if (j % batch_size) == 0:
+                w += ALPHA * (yd[j]-result)* np.array([1,x[j,0],x[j,1]])
+            
+        #plot_with_class(x,w,yd,"Widrow-Hoff")
+        
         erreur.append(e)
+        if (e == 0):
+            print("Epoch: ", i)
+            break
     return w, erreur
 
 
--- src/resources.py
+++ src/resources.py
@@ -1,7 +1,9 @@
 # Authors : Frejoux Gaetan, Niord Mathieu
 
 # Resources ########################################################
-URL_P2_D1 =          "../res/p2_d1.txt"            # p2_d1.txt
-URL_P2_D2 =          "../res/p2_d2.txt"            # p2_d2.txt
-URL_WANG  =          "../res/WangSignatures.xlsx"  # WangSignatures.xlsx
+URL_P2_D1 = "./res/p2_d1.txt"            # p2_d1.txt
+URL_P2_D2 = "./res/p2_d2.txt"            # p2_d2.txt
+URL_WANG  = "./res/WangSignatures.xlsx"  # WangSignatures.xlsx
+
+URL_SAVE_RES_E01 = "./res/Results/Exercice_01/"    # res_e01.txt
 ####################################################################
